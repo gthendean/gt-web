76bda1ffcaa8d69f3efd1b96d4ab11a1f72f5156
Gabriel Thendean <gabriel.thendean@sap.com>
2014-07-10 17:05:54 -0700

    invoice variable used twice
    Change-Id: Ic41768a3b875ce754f9025c23b4d5ab4490a153b

--- invoice.js
+++ invoice.js
@@ -1,4 +1,4 @@
-angular.module('invoice', ['finance'])
+angular.module('invoiceApp', ['finance'])
   .controller('InvoiceController', ['currencyConverter', function(currencyConverter) {
     this.qty = 1;
     this.cost = 2;
